51475cbb14961cdfa57194e29c2b42c21cc19d26
kinathru <kinath.ru@gmail.com>
2020-12-05 10:05:14 +0530

    feature branch

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         sdfsdfd There!
+         Feature Branch Development!
         </p>
         <a
           className="App-link"
